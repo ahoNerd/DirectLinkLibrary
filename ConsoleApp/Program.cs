1017e750e41d663cb15b8b7579f6b181e812b654
ahoNerd <ramdan@ahonerd.com>
2022-11-26 17:35:59 +0700

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿using Tugas11_0620204011;
+
+Hitung hitung = new Hitung();
+
+Console.WriteLine(hitung.Tambah(100, 200));
---
+++ Tugas11-0620204011/Hitung.cs
@@ -0,0 +1,11 @@
+﻿namespace Tugas11_0620204011
+{
+    public class Hitung
+    {
+        public int Tambah(int a, int b)
+        {
+            int hasil = a + b;
+            return hasil;
+        }
+    }
+}
